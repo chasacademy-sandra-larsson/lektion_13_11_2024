83e2707319daa520104cd0fe198b121b64a01906
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2024-11-13 10:52:51 +0100

    localstorage finished

--- localstorage/localstorage.js
+++ localstorage/localstorage.js
@@ -1,7 +1,48 @@
-//Local Storage
-
+// DOM referenser
 const input = document.getElementById("input");
 const save = document.getElementById("save");
 const clear = document.getElementById("clear");
-const output = document.getElementById("output");
+const basket = document.getElementById("basket");
+
+//Local Storaage har tre metoder setItem, getItem, clear
+
+// Kundkorgs-array som är tom från början
+let shoppingBasket = [];
+
+// Ladda aktuell kundkor från Local Storage
+const saveBasket = localStorage.getItem("shoppingBasket");
+if(saveBasket) {
+    shoppingBasket = JSON.parse(saveBasket)
+}
+
+save.addEventListener("click", () => {
+    // Sparar vår kundkorg i arbetsminnet
+    shoppingBasket.push(input.value);
+    // Använda LocalStorage för att spara en kundkorg i webbläsarminnet (Web API)
+    localStorage.setItem("shoppingBasket", JSON.stringify(shoppingBasket));
+    displayBasket();
+});
+
+function displayBasket() {
+    basket.textContent = `Kundkorgen har ${shoppingBasket.join(", ")}`;
+}
+
+clear.addEventListener("click", () => {
+    localStorage.removeItem("shoppingBasket");
+    //localStorage.clear(); // Rensar hela localSTorage
+    shoppingBasket = [];
+    basket.textContent = "";
+});
+
+displayBasket();
+
+
+
+
+
+
+
+
+
+
 
