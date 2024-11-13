c45c3d04f2a11eac6958fbc7b439fd6221524425
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2024-11-13 09:34:21 +0100

    added files

---
+++ localstorage/localstorage.js
@@ -0,0 +1,7 @@
+//Local Storage
+
+const input = document.getElementById("input");
+const save = document.getElementById("save");
+const clear = document.getElementById("clear");
+const output = document.getElementById("output");
+
